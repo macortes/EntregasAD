eb359227449259f62c009f3087338807e7841bec
macortes <miguelcortgil@gmail.com>
2015-11-10 08:44:24 +0100

    primer commit

---
+++ PArticulo/PArticulo/ArticuloView.cs
@@ -0,0 +1,39 @@
+using Gtk;
+using System;
+using System.Collections;
+using System.Data;
+using SerpisAd;
+
+namespace PArticulo
+{
+	public partial class ArticuloView : Gtk.Window
+	{
+		public ArticuloView () : 
+				base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+			//entryNombre.Text = "nuevo";
+			QueryResult queryResult = PersisterHelper.Get ("select * from categoria");
+			ComboBoxHelper.Fill (comboBoxCategoria, queryResult);
+			//spinButtonPrecio.Value = 1.5;
+
+			saveAction.Activated += delegate {	save();	};
+		}
+
+		private void save() {
+			IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand ();
+			dbCommand.CommandText = "insert into articulo (nombre, categoria, precio) " +
+				"values (@nombre, @categoria, @precio)";
+
+			string nombre = entryNombre.Text;
+			object categoria = ComboBoxHelper.GetId (comboBoxCategoria);
+			decimal precio = Convert.ToDecimal(spinButtonPrecio.Value);
+
+			DbCommandHelper.AddParameter (dbCommand, "nombre", nombre);
+			DbCommandHelper.AddParameter (dbCommand, "categoria", categoria);
+			DbCommandHelper.AddParameter (dbCommand, "precio", precio);
+			dbCommand.ExecuteNonQuery ();
+		}
+	}
+}
+
---
+++ PArticulo/PArticulo/MainWindow.cs
@@ -0,0 +1,35 @@
+using System;
+using Gtk;
+
+using SerpisAd;
+using PArticulo;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		Console.WriteLine ("MainWindow ctor.");
+		QueryResult queryResult = PersisterHelper.Get ("select * from articulo");
+		TreeViewHelper.Fill (treeView, queryResult);
+
+		newAction.Activated += delegate {
+			new ArticuloView();
+		};
+		//newAction.Activated += newActionActivated;
+	}
+
+//	void newActionActivated (object sender, EventArgs e)
+//	{
+//		new ArticuloView ();
+//	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+
+}
